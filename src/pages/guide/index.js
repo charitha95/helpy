8f88645ae3c9877865c7f1f08a6c102f4a7642f6
charitha95 <>
2020-03-01 19:09:02 +0530

    feature (guide): add guide page and styles

--- src/pages/guide/index.js
+++ src/pages/guide/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import guide_1 from '../../assets/imgs/no-connection-6.png'
+import guide_2 from '../../assets/imgs/taxi-waiting.png'
+import { Button, Carousel } from 'react-bootstrap';
 
 function Guide() {
   return (
-    <>
-      <h3>guide</h3>
-    </>
+    <div className='page-padding-x page-padding-y guide-page-wrapper'>
+      <div className="guide-page">
+        <div className='background-drop'>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={guide_1}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>We Listen</h3>
+                <p>we always hear your voice</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={guide_2}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>We Think</h3>
+                <p>we will think for you, don't worry</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className='actions'>
+          <Button variant="secondary">Get Help</Button>
+          <Button variant="secondary">Give Help</Button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 

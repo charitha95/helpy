a1d6c9f966ec70031e64780963af30694624a884
charitha95 <>
2020-03-01 19:13:21 +0530

    feature (guide): add as home page

--- src/app-routes/index.js
+++ src/app-routes/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Guide from '../pages/guide';
 import Welcome from '../pages/welcome';
-import NotFound from '../pages/404';
+// import NotFound from '../pages/404';
 
 
 function AppRoutes() {
   return (
     <Switch>
-      <Route exact path='/' component={NotFound} />
+      <Route exact path='/' component={Guide} />
       <Route exact path='/guide' component={Guide} />
       <Route exact path='/welcome' component={Welcome} />
     </Switch>

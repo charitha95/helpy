ae7f2c9693e951b1f3db5c49ad8c85ee6ced1106
charitha95 <>
2020-03-03 01:04:43 +0530

    feature (home): add tabbar and styles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,95 +7,109 @@ import interpersonal from '../../assets/imgs/taxi-coffee-break.png';
 import parenting from '../../assets/imgs/downloading-5.png';
 import finantial from '../../assets/imgs/payment-processed-4.png';
 import gender from '../../assets/imgs/taxi-no-connection.png';
-import { Row, Col } from 'react-bootstrap';
+import { ReactComponent as HomeIcon } from '../../assets/svg/home.svg';
+import { ReactComponent as ListIcon } from '../../assets/svg/list.svg';
+import { ReactComponent as UserIcon } from '../../assets/svg/user.svg';
+import { Row, Col, Tabs, Tab } from 'react-bootstrap';
 
 function Home() {
   return (
     <div className='page-padding-x page-padding-y page-wrapper white-background'>
-      <div className='home-page'>
-        <section className='title'>
-          <h3>Hello Dinu!</h3>
-          <p>How can we help you?</p>
-        </section>
-        <section className='components'>
-          <Row>
-            <Col>
-              <div className='component'>
-                <figure>
-                  <img src={relationship} alt='component' />
-                </figure>
-                <label>Relationship</label>
-              </div>
-            </Col>
-            <Col>
-              <div className='component'>
-                <figure>
-                  <img src={health} alt='component' />
-                </figure>
-                <label>Health Issues</label>
-              </div>
-            </Col>
-          </Row>
 
-          <Row>
-            <Col>
-              <div className='component'>
-                <figure>
-                  <img src={career} alt='component' />
-                </figure>
-                <label>Career</label>
-              </div>
-            </Col>
-            <Col>
-              <div className='component'>
-                <figure>
-                  <img src={family} alt='component' />
-                </figure>
-                <label>Family</label>
-              </div>
-            </Col>
-          </Row>
+      <Tabs defaultActiveKey="home">
+        <Tab eventKey="home" title={<HomeIcon />}>
+          <div className='home-page'>
+            <section className='title'>
+              <h3>Hello Dinu!</h3>
+              <p>How can we help you?</p>
+            </section>
+            <section className='components'>
+              <Row>
+                <Col>
+                  <div className='component'>
+                    <figure>
+                      <img src={relationship} alt='component' />
+                    </figure>
+                    <label>Relationship</label>
+                  </div>
+                </Col>
+                <Col>
+                  <div className='component'>
+                    <figure>
+                      <img src={health} alt='component' />
+                    </figure>
+                    <label>Health Issues</label>
+                  </div>
+                </Col>
+              </Row>
 
-          <Row>
-            <Col>
-              <div className='component'>
-                <figure>
-                  <img src={interpersonal} alt='component' />
-                </figure>
-                <label>Interpersonal</label>
-              </div>
-            </Col>
-            <Col>
-              <div className='component'>
-                <figure>
-                  <img src={parenting} alt='component' />
-                </figure>
-                <label>Parenting</label>
-              </div>
-            </Col>
-          </Row>
+              <Row>
+                <Col>
+                  <div className='component'>
+                    <figure>
+                      <img src={career} alt='component' />
+                    </figure>
+                    <label>Career</label>
+                  </div>
+                </Col>
+                <Col>
+                  <div className='component'>
+                    <figure>
+                      <img src={family} alt='component' />
+                    </figure>
+                    <label>Family</label>
+                  </div>
+                </Col>
+              </Row>
 
-          <Row>
-            <Col>
-              <div className='component'>
-                <figure>
-                  <img src={finantial} alt='component' />
-                </figure>
-                <label>Finantial</label>
-              </div>
-            </Col>
-            <Col>
-              <div className='component'>
-                <figure>
-                  <img src={gender} alt='component' />
-                </figure>
-                <label>Gender Identity</label>
-              </div>
-            </Col>
-          </Row>
+              <Row>
+                <Col>
+                  <div className='component'>
+                    <figure>
+                      <img src={interpersonal} alt='component' />
+                    </figure>
+                    <label>Interpersonal</label>
+                  </div>
+                </Col>
+                <Col>
+                  <div className='component'>
+                    <figure>
+                      <img src={parenting} alt='component' />
+                    </figure>
+                    <label>Parenting</label>
+                  </div>
+                </Col>
+              </Row>
 
-        </section>
-      </div>
+              <Row>
+                <Col>
+                  <div className='component'>
+                    <figure>
+                      <img src={finantial} alt='component' />
+                    </figure>
+                    <label>Finantial</label>
+                  </div>
+                </Col>
+                <Col>
+                  <div className='component'>
+                    <figure>
+                      <img src={gender} alt='component' />
+                    </figure>
+                    <label>Gender Identity</label>
+                  </div>
+                </Col>
+              </Row>
+
+            </section>
+          </div>
+        </Tab>
+        <Tab eventKey="profile" title={<ListIcon />}>
+          <p>Profile</p>
+        </Tab>
+        <Tab eventKey="contact" title={<UserIcon />}>
+          <p>Contact</p>
+        </Tab>
+      </Tabs>
     </div>
   )
 }

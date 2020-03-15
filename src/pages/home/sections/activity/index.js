2ef2f32336b26eb41389db1de3de16a541b20c42
charitha95 <>
2020-03-15 20:06:50 +0530

    feature (home): separate categories from home

---
+++ src/pages/home/sections/activity/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Activity = () => {
+  return (
+    <div>
+      from Activity
+    </div>
+  )
+}
+
+export default Activity;
---
+++ src/pages/time-line/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default Timeline = () => {
+  return (
+    <div>hey</div>
+  )
+}

785937e015d0e6efba7f040491d3ff920d9b51ab
charitha95 <>
2020-03-29 14:44:30 +0530

    feature (user): get username from db

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactComponent as HomeIcon } from '../../assets/svg/home.svg';
 import { ReactComponent as ListIcon } from '../../assets/svg/list.svg';
 import { ReactComponent as UserIcon } from '../../assets/svg/user.svg';
@@ -6,8 +6,16 @@ import { Tabs, Tab } from 'react-bootstrap';
 import { Activity, Category } from './sections'
 import { signOut } from '../../helpers/auth';
 import { withRouter } from 'react-router-dom';
+import { auth, db } from '../../services/firebase';
 
-function Home({history}) {
+function Home({ history }) {
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    db.ref(`users/${auth().currentUser.uid}/display_name`).on('value',
+      snap => setUserName(snap.val()))
+  }, []);
+  
   const logout = (e) => {
     e.preventDefault();
     signOut();
@@ -16,19 +24,26 @@ function Home({history}) {
   const goTo = (val) => {
     history.push(val)
   }
+
+  const loadUser = () => {
+    console.log(auth().currentUser)
+
+  }
+
   return (
     <div className='page-padding-x page-padding-y page-wrapper white-background'>
 
       <Tabs defaultActiveKey="home">
         <Tab eventKey="home" title={<HomeIcon />}>
-          <Category />
+          <Category userName={userName} />
         </Tab>
         <Tab eventKey="activity" title={<ListIcon />}>
           <Activity />
         </Tab>
         <Tab eventKey="contact" title={<UserIcon />}>
           <button onClick={(e) => logout(e)}>log out</button>
-          <button onClick={(e) => goTo('/emergency-contact')}>go to emergancy</button>
+          <button onClick={() => goTo('/emergency-contact')}>go to emergancy</button>
+          <button onClick={() => loadUser()}>load user</button>
         </Tab>
       </Tabs>
     </div>

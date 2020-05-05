2487b8055cc168b6cfac31e83107c80b29dbe91d
charitha95 <charithag@99x.lk>
2020-05-05 19:06:54 +0530

    feature (chart): add cahrt

--- src/pages/home/sections/profile/index.js
+++ src/pages/home/sections/profile/index.js
@@ -5,9 +5,74 @@ import GetTone from './sections/tone';
 
 
 import UserChart from './sections/chart';
-import Speech from './sections/voice';
+// import Speech from './sections/voice';
+import { db, auth } from '../../../../services/firebase';
 
 class Profile extends Component {
+  state = {
+    calls: []
+  }
+  componentDidMount() {
+    db.ref(`users/${auth().currentUser.uid}`).once("value", user => {
+      console.log(user.val())
+      if (user.val().calls) {
+        this.setState({ calls: user.val().calls }, () => {
+          this.getRates();
+        });
+      } else {
+        console.log('no calls found')
+      }
+      // const toneParams = {
+      //   toneInput: { 'text': latesUserThoughts },
+      //   contentType: 'application/json',
+      // };
+
+      // toneAnalyzer.tone(toneParams)
+      //   .then(toneAnalysis => {
+      //     console.log(toneAnalysis.result)
+      //     setTones(toneAnalysis.result.document_tone.tones);
+      //     setCanStart(true);
+      //   })
+      //   .catch(err => {
+      //     console.log('error:', err);
+      //   });
+    });
+  }
+
+  getRates = () => {
+    const scoreArr = [];
+    const titleArr = [];
+    if (this.state.calls) {
+      this.state.calls.forEach(call => {
+        let score = 0;
+        call.tonesAfterCall.map(tone => {
+          switch (tone.tone_id) {
+            case 'sadness':
+              return score += -5;
+            case 'fear':
+              return score += -10;
+            case 'anger':
+              return score += -5;
+            case 'tentative':
+              return score += 2;
+            case 'joy':
+              return score += 10;
+            case 'confident':
+              return score += 15;
+            case 'analytical':
+              return score += 5;
+            default:
+              return score;
+          }
+        });
+        console.log(score)
+        scoreArr.push(score);
+        titleArr.push(call.date.replace('-2020', ''))
+      })
+    }
+    console.log(scoreArr)
+    console.log(titleArr)
+  }
 
   render() {
     return <div className='profile-page'>
@@ -24,9 +89,9 @@ class Profile extends Component {
           <Tab eventKey="feed" title='Stats' >
             <GetTone />
           </Tab>
-          <Tab eventKey="activity" title='Speech'>
+          {/* <Tab eventKey="activity" title='Speech'>
            <Speech/>
-          </Tab>
+          </Tab> */}
         </Tabs>
       </section>
 
--- src/pages/home/sections/profile/sections/chart.js
+++ src/pages/home/sections/profile/sections/chart.js
@@ -2,29 +2,23 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 
 const UserChart = props => {
-    const data = {
-        labels: ['april-27', 'april-29', 'april-28', 'may-1', 'may-2'],
-        datasets: [{
-            label: 'Happiness',
-            data: [1, 2, 4, 4, 6],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1
-        }]
-    }
+  const data = {
+    labels: ['april-27', 'april-29', 'april-28', 'may-1', 'may-2'],
+    datasets: [{
+      label: 'Happiness',
+      data: [1, 2, 4, 4, 6],
+      backgroundColor: [
+        'rgba(99, 255, 110, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)'
+      ],
+      borderWidth: 1
+    }]
+  }
 
 
-    return <Line data={data} />
+  return <Line data={data} />
 }
 
 export default UserChart;
